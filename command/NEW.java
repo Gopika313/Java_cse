a2237668e88fdb76ba36ea50b9d76bb4d02ee864
Gopika313 <gopikagkurup18@gmail.com>
2025-03-12 14:54:46 +0530

    :welcome

---
+++ command/NEW.java
@@ -0,0 +1,10 @@
+public class NEW{
+    /*
+    1)NEW file
+    2)using variables
+    */
+    public static void main(String args[]){
+        int num = 10; //this is int value
+        System.out.println(num);
+    }
+}
