2309276ef0ef0c7e40a684871faaac18107eeea5
Gopika313 <gopikagkurup18@gmail.com>
2025-03-12 15:24:54 +0530

    :hello guys

---
+++ Method/INT.java
@@ -0,0 +1,13 @@
+public class INT{
+
+    public void add(int numOne,int numTwo){
+        int numThree=numOne+numTwo;
+        System.out.println(numThree);
+    }
+    public static void main(String args[]){
+        int numOne=10;
+        System.out.println(numOne);
+    INT obj=new INT();
+    obj.add(10,20);
+    }
+}
