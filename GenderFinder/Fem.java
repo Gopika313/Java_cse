5c8db3c266f68e23aad4a4a480640df9aaeb82f9
Gopika313 <gopikagkurup18@gmail.com>
2025-03-13 11:34:02 +0530

    :this is genderfinder

---
+++ GenderFinder/Fem.java
@@ -0,0 +1,12 @@
+public class Fem{
+    public static void main(String args[]){
+        char Gender='m';
+        if(Gender=='m'){
+        System.out.println("it is male");
+        }
+        else {
+        System.out.println("it is female");
+         }
+          }
+                
+}
