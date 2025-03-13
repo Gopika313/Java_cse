604fce58d0cf74406d14c48e4504a4ad6fd41171
Gopika313 <gopikagkurup18@gmail.com>
2025-03-13 11:20:30 +0530

    :this is pos and neg

---
+++ PositiveNegative/Sys.java
@@ -0,0 +1,11 @@
+public class Sys{
+    public static void main(String args[]){
+        int x =35;
+        if(x>=0){
+        System.out.println("Positive");
+        }
+        else
+        System.out.println("Negative");
+            }
+                
+}
