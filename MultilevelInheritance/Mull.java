5681e1046f8beaa9f263aefd34b2b361ddc8cbcc
Gopika313 <gopikagkurup18@gmail.com>
2025-03-14 13:50:10 +0530

    :this is multilevel

---
+++ MultilevelInheritance/Mull.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Parent{
+     public void add(){
+     System.out.println("hello world");
+     }
+}
+class Child extends Parent {
+    void display(){
+        add();
+    }
+
+}
+class Third extends Child{
+    void helper(){
+        display();
+    }
+}
+public class Mull{
+    public static void main (String args[]){
+       Third obj = new Third();
+       obj.helper(); 
+    }
+      
+}
