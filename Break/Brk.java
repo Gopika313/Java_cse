4eed2b2b03a82a6c3738d91f6f56ebd45bd128df
Gopika313 <gopikagkurup18@gmail.com>
2025-03-14 14:13:14 +0530

    : this is even number under 5

---
+++ Break/Brk.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Brk{
+    public static void main(String args[]){
+        int i=0;
+        while(i<10){
+            if(i%2==0){
+                if(i>5){
+                    break;
+                }
+                System.out.println(i);
+            }
+            i=i+1;
+
+        }
+    }
+}
