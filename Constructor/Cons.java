ac4e089cb6ed8ebffd89a1249f3bf0cd427598dc
Gopika313 <gopikagkurup18@gmail.com>
2025-03-15 10:45:42 +0530

    : this is Constructor

---
+++ Constructor/Cons.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Cons{
+    Cons(){
+        System.out.println("aabbcc");
+
+    }
+    public static void main(String args[]){
+       Cons obj = new Cons();  
+    }
+}
